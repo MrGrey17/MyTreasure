dae5f17f8a25e3fd9bd4c0972a5eb2fa0ed5799b
KristalMan <kirnosov082@gmail.com>
2020-12-10 13:47:47 +0200

    First changes in front

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,139 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>HomePage</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        <div id="listArticles"></div>
+        <div class="list-articles">
+            <div class="article">
+                <div class="photo">
+                    <img src="#">
+                </div>
+                <div class="article-title">
+                    <h1>Test Article</h1>
+                </div>
+                <div class="article-description">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore commodi doloremque aspernatur eos atque aliquam amet consequatur impedit officiis beatae veritatis, laudantium voluptates mollitia, molestiae distinctio ab placeat saepe quas.</p>
+                </div>
+                <div class="activity">
+                    <div class="views">
+                        <img src="#">
+                        <p>143</p>
+                    </div>
+                    <div class="likes">
+                        <img src="#">
+                        <p>12</p>
+                    </div>
+                    <div class="comments">
+                        <img src="#">
+                        <p>3</p>
+                    </div>
+                </div>
+            </div>
+            <div class="article">
+                <div class="photo">
+                    <img src="#">
+                </div>
+                <div class="article-title">
+                    <h1>Test Article</h1>
+                </div>
+                <div class="article-description">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore commodi doloremque aspernatur eos atque aliquam amet consequatur impedit officiis beatae veritatis, laudantium voluptates mollitia, molestiae distinctio ab placeat saepe quas.</p>
+                </div>
+                <div class="activity">
+                    <div class="views">
+                        <img src="#">
+                        <p>143</p>
+                    </div>
+                    <div class="likes">
+                        <img src="#">
+                        <p>12</p>
+                    </div>
+                    <div class="comments">
+                        <img src="#">
+                        <p>3</p>
+                    </div>
+                </div>
+            </div>
+            <div class="article">
+                <div class="photo">
+                    <img src="#">
+                </div>
+                <div class="article-title">
+                    <h1>Test Article</h1>
+                </div>
+                <div class="article-description">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore commodi doloremque aspernatur eos atque aliquam amet consequatur impedit officiis beatae veritatis, laudantium voluptates mollitia, molestiae distinctio ab placeat saepe quas.</p>
+                </div>
+                <div class="activity">
+                    <div class="views">
+                        <img src="#">
+                        <p>143</p>
+                    </div>
+                    <div class="likes">
+                        <img src="#">
+                        <p>12</p>
+                    </div>
+                    <div class="comments">
+                        <img src="#">
+                        <p>3</p>
+                    </div>
+                </div>
+            </div>
+            <div class="article">
+                <div class="photo">
+                    <img src="#">
+                </div>
+                <div class="article-title">
+                    <h1>Test Article</h1>
+                </div>
+                <div class="article-description">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore commodi doloremque aspernatur eos atque aliquam amet consequatur impedit officiis beatae veritatis, laudantium voluptates mollitia, molestiae distinctio ab placeat saepe quas.</p>
+                </div>
+                <div class="activity">
+                    <div class="views">
+                        <img src="#">
+                        <p>143</p>
+                    </div>
+                    <div class="likes">
+                        <img src="#">
+                        <p>12</p>
+                    </div>
+                    <div class="comments">
+                        <img src="#">
+                        <p>3</p>
+                    </div>
+                </div>
+            </div>
+            <div class="article">
+                <div class="photo">
+                    <img src="#">
+                </div>
+                <div class="article-title">
+                    <h1>Test Article</h1>
+                </div>
+                <div class="article-description">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore commodi doloremque aspernatur eos atque aliquam amet consequatur impedit officiis beatae veritatis, laudantium voluptates mollitia, molestiae distinctio ab placeat saepe quas.</p>
+                </div>
+                <div class="activity">
+                    <div class="views">
+                        <img src="#">
+                        <p>143</p>
+                    </div>
+                    <div class="likes">
+                        <img src="#">
+                        <p>12</p>
+                    </div>
+                    <div class="comments">
+                        <img src="#">
+                        <p>3</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
-    <script src="{{ asset('js/app.js') }}"></script>
 </html>

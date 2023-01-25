279de255269ce6a945a5adad3c991bf8a8384ac3
MrGrey17 <kirnosov082@gmail.com>
2023-01-25 22:18:12 +0200

    Fixes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,49 +10,47 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                @foreach ($errors->all() as $error)
+                    <div>{{ $error }}</div>
+                @endforeach
+            </div>
+        @endif
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            @foreach ($errors->all() as $error)
-                <div>{{ $error }}</div>
-            @endforeach
-        </div>
-    @endif
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
 
-    @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+        @if (session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
 
-    @if (session('error'))
-        <div class="alert alert-danger">
-            {{ session('error') }}
-        </div>
-    @endif
+        <form action="/login" method="post">
+            {{ csrf_field() }}
 
-    <form action="/login" method="post">
-        {{ csrf_field() }}
+            <div class="form-group">
+                <label for="email">
+                    Email:
+                </label>
+                <input type="email" class="form-control" name="email" placeholder="Type your Email here:">
+            </div>
 
-        <div class="form-group">
-            <label for="email">
-                Email:
-            </label>
-            <input type="email" class="form-control" name="email" placeholder="Type your Email here:">
-        </div>
+            <div class="form-group">
+                <label for="password">
+                    Password:
+                </label>
+                <input type="password" class="form-control" name="password" placeholder="Type your Password here:">
+            </div>
 
-        <div class="form-group">
-            <label for="password">
-                Password:
-            </label>
-            <input type="password" class="form-control" name="password" placeholder="Type your Password here:">
-        </div>
+            <button type="submit" class="btn btn-primary">
+                Login
+            </button>
 
-        <button type="submit" class="btn btn-primary">
-            Login
-        </button>
-
-    </form>
-
+        </form>
     </body>
 </html>
